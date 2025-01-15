0cce6b28e735cfc7d3460d43cb77da6c17570d16
Haelnorr <lockedonohoe@gmail.com>
2025-01-15 14:01:48 +1100

    added flask-migrate and began work on database

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_smorest import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from src.logger import get_logger
 from src.api.config import Config
 
 log = get_logger(__name__)
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config_class=Config):
     log.info("Creating flask app")
@@ -11,7 +16,8 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
     api = Api(app)
 
-    # do stuff here
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from src.api.blueprints.movies import bp as movies_bp
     api.register_blueprint(movies_bp)
--- backend/src/run.py
+++ backend/src/run.py
@@ -7,3 +7,17 @@ from src.api import create_app
 app = create_app()
 log.info('Accepting requests')
 
+
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from src.api import db
+from src.api.models import User
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'sa': sa,
+        'so': so,
+        'db': db,
+        'User': User
+    }
