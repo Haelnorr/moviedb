cb92376e268d424e34d9e20080d1972d018444ec
Haelnorr <lockedonohoe@gmail.com>
2025-02-01 19:16:17 +1100

    Added react-compiler

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   serverExternalPackages: ["pino", "pino-pretty"],
   output: "standalone",
+  experimental: { reactCompiler: true },
 };
 
 export default nextConfig;
