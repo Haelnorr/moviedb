3250d6d877af7b2adcf0a269827d259be6947199
Haelnorr <lockedonohoe@gmail.com>
2025-02-01 20:35:52 +1100

    Updated landing page

--- README.md
+++ README.md
@@ -1,37 +1,19 @@
-# moviedb
+# MovieDB
 
+### AKA the Untitled Movie Review Site
+Welcome to the Untitled Movie Review website. The idea for this
+project is to offer a new take on movie reviews. To do that, there
+are two problems this project aims to provide solutions to.
 
-BACKEND:
+The first is a better aggregate rating system. The current norm
+for rating systems is bland at best, and unhelpful at worst. Users
+deserve a better way to see what other people thought of the
+movies they're looking up.
 
+The second is a better way to discover new movies. Deciding on a
+new movie to watch can be difficult, with potential spoilers
+looming behind every review, and too many options to choose.
 
-Testing requires redis on default port, and postgres server on port 5433 with 
-credentials "user:password".  
-Run the test with `./runtest` from inside the venv, or `./coveragehtml` to run 
-the test and generate a html report showing the coverage.
+This project is still in its infancy, so for now those solutions
+are just ideas, but hopefully soon they'll be a reality,
 
-
-BACKEND ENVARS:  
-```
-SECRET_KEY
-LOG_DIR
-LOG_LEVEL
-REDIS
-REDIS_PORT
-JWT_REDIS_INDEX
-POSTGRES_USER
-POSTGRES_PASS
-POSTGRES_HOST
-POSTGRES_PORT
-POSTGRES_SSL_MODE
-POSTGRES_ENDPOINT (optional)
-POSTGRES_DB_NAME
-```
-
-FRONTEND ENVARS:  
-```
-API_URL
-API_PORT
-COOKIE_SECURE_MODE
-NODE_ENV
-LOG_LEVEL
-```
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,54 @@
+import styles from "./styles.module.css";
 const Home = () => {
   return (
-    <main>
-      <h1 className="text-danger">Hello Bootstrap</h1>
-    </main>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div className={styles.title}>MovieDB</div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <div className={styles.tagline}>
+            AKA the Untitled Movie Review Site
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <div className={styles.body}>
+            <div className={styles.paragraph}>
+              Welcome to the Untitled Movie Review website. The idea for this
+              project is to offer a new take on movie reviews. To do that, there
+              are two problems this project aims to provide solutions to.
+            </div>
+            <div className={styles.paragraph}>
+              The first is a better aggregate rating system. The current norm
+              for rating systems is bland at best, and unhelpful at worst. Users
+              deserve a better way to see what other people thought of the
+              movies they're looking up.
+            </div>
+            <div className={styles.paragraph}>
+              The second is a better way to discover new movies. Deciding on a
+              new movie to watch can be difficult, with potential spoilers
+              looming behind every review, and too many options to choose.
+            </div>
+            <div className={styles.paragraph}>
+              This project is still in its infancy, so for now those solutions
+              are just ideas, but hopefully soon they'll be a reality,
+            </div>
+            <div className={styles.paragraph}>
+              <a
+                href="https://github.com/Haelnorr/moviedb"
+                className={styles.gitlink}
+              >
+                Check out the Github repo
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
